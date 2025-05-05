70977bc8799b3835847134337b0934d94c8ea49c
Anthony Isensee <aisensee@adventistrisk.org>
2025-05-05 16:55:29 -0400

    chore: adds basic modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useSignal, useSignals } from '@preact/signals-react/runtime'
 import './App.css'
 
+const vimeoEmbeds = [
+  <iframe title="vimeo-player" src="https://player.vimeo.com/video/950060620?h=e40cf979c5" width="640" height="360" frameborder="0"    allowfullscreen></iframe>,
+  <iframe title="vimeo-player" src="https://player.vimeo.com/video/936429453?h=6782698888" width="640" height="360" frameborder="0"    allowfullscreen></iframe>,
+  <iframe title="vimeo-player" src="https://player.vimeo.com/video/895947404?h=29b1c7259d" width="640" height="360" frameborder="0"    allowfullscreen></iframe>
+]
+
 function App() {
-  const [count, setCount] = useState(0)
+
+  useSignals()
+
+  const modalPlayerIsVisible = useSignal(true)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <section className="section">
+        <div className="content">
+          <h1>Video Kiosk</h1>
+        </div>
+      </section>
+      <section className="section">
+        <div className="grid">
+          
+        </div>
+      </section>
+      <div className={`modal ${modalPlayerIsVisible.value && 'is-active'}`}>
+        <div className="modal-background"></div>
+        <div className="modal-content">
+          {vimeoEmbeds[0]}
+        </div>
+        <button className="modal-close is-large" aria-label="close" onClick={() => modalPlayerIsVisible.value = false}></button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
+
   )
+
 }
 
 export default App
