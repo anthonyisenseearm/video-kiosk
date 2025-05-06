7ab7891ba45f6cf536a3f1f424ce2f98bd87606c
Anthony Isensee <aisensee@adventistrisk.org>
2025-05-06 10:18:48 -0400

    feat: adds cell layout and media modal open/close

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,32 @@
 import { useSignal, useSignals } from '@preact/signals-react/runtime'
+import { Signal } from '@preact/signals-react'
 import './App.css'
+import type { ReactElement } from 'react'
 
 const vimeoEmbeds = [
-  <iframe title="vimeo-player" src="https://player.vimeo.com/video/950060620?h=e40cf979c5" width="640" height="360" frameborder="0"    allowfullscreen></iframe>,
-  <iframe title="vimeo-player" src="https://player.vimeo.com/video/936429453?h=6782698888" width="640" height="360" frameborder="0"    allowfullscreen></iframe>,
-  <iframe title="vimeo-player" src="https://player.vimeo.com/video/895947404?h=29b1c7259d" width="640" height="360" frameborder="0"    allowfullscreen></iframe>
+  <iframe title="vimeo-player" src="https://player.vimeo.com/video/950060620?h=e40cf979c5" width="640" height="360"></iframe>,
+  <iframe title="vimeo-player" src="https://player.vimeo.com/video/936429453?h=6782698888" width="640" height="360"></iframe>,
+  <iframe title="vimeo-player" src="https://player.vimeo.com/video/895947404?h=29b1c7259d" width="640" height="360"></iframe>
 ]
 
 function App() {
 
   useSignals()
 
-  const modalPlayerIsVisible = useSignal(true)
+  console.log(vimeoEmbeds)
+
+  const mediaModalIsActive: Signal<boolean> = useSignal(false)
+  const mediaModalVimeoEmbed: Signal<ReactElement> = useSignal(<></>)
+
+  function openMediaModal(embed: ReactElement): void {
+    mediaModalVimeoEmbed.value = embed
+    mediaModalIsActive.value = true
+  }
+
+  function closeMediaModal(): void {
+    mediaModalVimeoEmbed.value = <></>
+    mediaModalIsActive.value = false
+  }
 
   return (
     <>
@@ -22,15 +37,25 @@ function App() {
       </section>
       <section className="section">
         <div className="grid">
-          
+          {vimeoEmbeds.map((embed, index) => <>
+            <div className='cell'>
+              <div className="box">
+                <div className="content">
+                  <h3>Video {index}</h3>
+                  <button className="button" onClick={() => openMediaModal(embed)}>View</button>
+                </div>
+              </div>
+            </div>
+          </>)}
+          <div className="cell"></div>
         </div>
       </section>
-      <div className={`modal ${modalPlayerIsVisible.value && 'is-active'}`}>
+      <div className={`modal ${mediaModalIsActive.value && 'is-active'}`}>
         <div className="modal-background"></div>
         <div className="modal-content">
-          {vimeoEmbeds[0]}
+          {mediaModalVimeoEmbed.value}
         </div>
-        <button className="modal-close is-large" aria-label="close" onClick={() => modalPlayerIsVisible.value = false}></button>
+        <button className="modal-close is-large" aria-label="close" onClick={closeMediaModal}></button>
       </div>
     </>
 
