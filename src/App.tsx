e9ecce06d4d10ca89c4fe2f014a23bc28e872491
Anthony Isensee <aisensee@adventistrisk.org>
2025-07-17 16:47:27 -0400

    style: adds responsive layout for horizontal or vertical orientation

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function Modal({ content, isActive, onClickClose }: ModalOptions) {
 
   return (
     <>
-      <div className={`modal ${isActive && 'is-active'}`}>
+      <div className={`modal ${isActive ? 'is-active' : ""}`}>
         <div className="modal-background" onClick={() => onClickClose()}></div>
         <div className="modal-content" style={{ width: "80rem" }}>
           {content}
@@ -57,24 +57,22 @@ function App() {
 
   return (
     <>
-      <section className="section">
-        <div className="container mb-5">
-          <div className="is-flex is-justify-content-center">
-            <ARMLogo />
-            <div className="is-flex is-flex-direction-column is-justify-content-center">
-              <div className="content ml-5">
-                <h1>Adventist Risk Management<sup>®</sup>, Inc.</h1>
-                <p className='subtitle'>Tap any of the cards below to watch a video!</p>
-              </div>
+      <div className="p-6 is-flex is-flex-direction-column is-justify-content-space-around" style={{ minHeight: "100vh" }}>
+        <div className="is-flex is-justify-content-center" style={{ minHeight: "15vh" }}>
+          <ARMLogo />
+          <div className="is-flex is-flex-direction-column is-justify-content-center">
+            <div className="content ml-5">
+              <h1>Adventist Risk Management<sup>®</sup>, Inc.</h1>
+              <p className='subtitle'>Tap any of the cards below to watch a video!</p>
             </div>
           </div>
         </div>
-        <div className="container">
-          <div className="fixed-grid has-4-cols">
-            <div className="grid is-gap-4">
+        <div>
+          <div className="container">
+            <div className="is-flex is-flex-direction-row is-flex-wrap-wrap is-justify-content-center	">
               {newVimeoEmbeds.map((vimeoEmbed, index) =>
-                <div className='cell' onClick={() => openMediaModal(index)} style={{ cursor: "pointer" }} key={index}>
-                  <div className="box is-flex is-flex-direction-column is-justify-content-space-between" style={{ minHeight: "100%" }}>
+                <div className='m-4' onClick={() => openMediaModal(index)} style={{ cursor: "pointer" }} key={index}>
+                  <div className="box is-flex is-flex-direction-column is-justify-content-space-between" style={{ minHeight: "100%", maxWidth: "19rem" }}>
                     <div className="content">
                       <figure className="is-16by9">
                         <img src='https://bulma.io/assets/images/placeholders/640x360.png'></img>
@@ -89,10 +87,14 @@ function App() {
                 </div>
               )}
             </div>
-            <div className="cell"></div>
           </div>
         </div>
-      </section>
+        <div className='is-flex is-flex-direction-column is-justify-content-center' style={{ minHeight: "15vh" }}>
+          <div className="content has-text-centered has-text-grey">
+            <p>&copy; {new Date().getFullYear()} Adventist Risk Management<sup>®</sup>, Inc.</p>
+          </div>
+        </div>
+      </div>
       {newVimeoEmbeds.map((vimeoEmbed, index) => <div className={`modal-${index}`} key={index}>
         <Modal
           isActive={index === mediaModalActiveIndex.value}
